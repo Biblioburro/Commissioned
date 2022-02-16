fb1a0e9797e950f161458bc3e5a43d64326a247a
Samuel Megginson <38111067+Biblioburro@users.noreply.github.com>
2022-02-16 16:46:45 -0500

    Update bruh.js

--- bruh.js
+++ bruh.js
@@ -2,7 +2,7 @@ var numDivs = document.getElementsByTagName("div").length;
 var divs =document.getElementsByTagName("div");
 let colorArr = ['red','green','blue']; 
 let tagArr = ['p','span','H1','H2','H3','H4','H5','H6','td','li'];
-
+var imgURL = "https://c.tenor.com/Nbg6A9cVK4gAAAAC/cry-about-it-rickroll.gif"
 function replaceArrTagsHTML(){
 
 	for(let j = 0 ; j<tagArr.length;j++){
@@ -31,14 +31,14 @@ elements[i].innerHTML = "bruh";
 }
 }
 function rickHtmlLinks(){
-var imgurl = "https://c.tenor.com/Nbg6A9cVK4gAAAAC/cry-about-it-rickroll.gif"
+var imgurl = imgURL;
 var elements = document.getElementsByTagName("a");
 for(let i = 0 ; i< elements.length;i++){
 elements[i].href = imgurl;
 }
 }
 function rickHtmlImgs(){
-var imgurl = "https://c.tenor.com/Nbg6A9cVK4gAAAAC/cry-about-it-rickroll.gif"
+var imgurl = imgURL;
 var elements = document.getElementsByTagName("img");
 for(let i = 0 ; i< elements.length;i++){
 elements[i].src = imgurl;
